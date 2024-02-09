7b58fcfc683d0dca6ea4c2c6308d9e55e2b66207
Jared Ricks <jvaun20@gmail.com>
2024-02-09 14:26:02 -0500

    Added Data Persistence
    - Save data! Yay!

---
+++ Assets/Code/Scripts/DataPersistence/DataPersistenceManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DataPersistenceManager : MonoBehaviour
+{
+    [Header("File Storage Config")]
+    [SerializeField] private string fileName;
+
+    private GameData gameData;
+    private List<IDataPersistence> dataPersistenceObjects;
+    private FileDataHandler dataHandler;
+
+    public static DataPersistenceManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found more than one Data Persistence Manager in the scene.");
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        this.dataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
+        this.dataPersistenceObjects = FindAllDataPersistenceObjects();
+        LoadGame();
+    }
+
+    public void NewGame()
+    {
+        this.gameData = new GameData();
+    }
+
+    public void LoadGame()
+    {
+        this.gameData = dataHandler.Load();
+
+        if (this.gameData == null)
+        {
+            Debug.Log("No data was found. Initializing to defaults.");
+            NewGame();
+        }
+
+        foreach (IDataPersistence dataPersistenceObj in dataPersistenceObjects)
+        {
+            dataPersistenceObj.LoadData(gameData);
+        }
+
+        Debug.Log("Loaded coin amount: " + gameData.coins);
+    }
+
+    public void SaveGame()
+    {
+        foreach (IDataPersistence dataPersistenceObj in dataPersistenceObjects)
+        {
+            dataPersistenceObj.SaveData(ref gameData);
+        }
+
+        Debug.Log("Saved coin amount: " + gameData.coins);
+
+        dataHandler.Save(gameData);
+    }
+
+    private List<IDataPersistence> FindAllDataPersistenceObjects()
+    {
+        IEnumerable<IDataPersistence> dataPersistenceObjects = FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None).OfType<IDataPersistence>();
+        return new List<IDataPersistence>(dataPersistenceObjects);
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveGame();
+    }
+}
