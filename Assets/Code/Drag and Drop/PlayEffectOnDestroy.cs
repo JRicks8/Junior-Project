adedc7b781c73e2a149441a17127001c2f87c779
Hank Kauffman <43581135+Hankik@users.noreply.github.com>
2024-02-12 14:51:15 -0500

    CopyComponent + PlayEffectOnDestroy
    -Added a utility function to copy components procedurally
    -Added a script for playing effects on object destruction
    -Added prefab object to spawn in and store effect component when objects with playeffectondestroy script die

---
+++ Assets/Code/Drag and Drop/PlayEffectOnDestroy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayEffectOnDestroy : MonoBehaviour
+{
+    [Header("Required: Attach the blue cube labeled\n'EffectSourceObject' from the DragAndDrop folder.")]
+    [SerializeField] private GameObject m_EffectSourceObjectPrefab;
+
+    [Header("Required: Attach a new component of type 'Particle System'\nthen drag from the created component into here.")]
+    [SerializeField] private ParticleSystem m_EffectSourceComponent;
+
+    private void OnDestroy()
+    {
+        GameObject spawnedObject = Instantiate(m_EffectSourceObjectPrefab, transform.position, transform.rotation, null);
+        spawnedObject.CopyComponent(m_EffectSourceComponent);
+        ParticleSystem spawnedComponent = spawnedObject.GetComponent<ParticleSystem>();
+        if (!spawnedComponent || spawnedComponent.isPlaying) return;
+        spawnedComponent.Play();
+    }
+}
