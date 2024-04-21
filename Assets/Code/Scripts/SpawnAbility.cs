5accc4fa05cf5e0bbb7434406b06d19d8ffeba1d
Hank Kauffman <43581135+Hankik@users.noreply.github.com>
2024-04-21 12:47:50 -0400

    hand anim stuff
    messing with hand anim

---
+++ Assets/Code/Scripts/SpawnAbility.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class SpawnAbility : MonoBehaviour
+{
+    [SerializeField] private GameObject ability;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && other.GetComponent<PlayerController>().HasPackage())
+        {
+            ability.SetActive(true);
+        }
+    }
+}
